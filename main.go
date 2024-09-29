bb7df3b9e7be39266db47491c252b3cb7b4e283a
kamil713 <kam.pietrz@gmail.com>
2024-09-29 19:45:02 +0200

    feat: add RSS fetch logic + handler with HTMX template

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/xml"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"text/template"
@@ -32,8 +34,53 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 	tmpl.Execute(w, nil)
 }
 
+// Function to fetch and parse the RSS feed
+func fetchRSSFeed(feedURL string) (*RSS, error) {
+	// Fetch the feed data
+	resp, err := http.Get(feedURL)
+	if err != nil {
+		return nil, fmt.Errorf("could not fetch RSS feed: %v", err)
+	}
+	defer resp.Body.Close()
+
+	// Read the response body
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("could not read response body: %v", err)
+	}
+
+	// Parse the XML into our RSS struct
+	var rss RSS
+	err = xml.Unmarshal(data, &rss)
+	if err != nil {
+		return nil, fmt.Errorf("could not parse RSS feed: %v", err)
+	}
+
+	return &rss, nil
+}
+
+// Fetch RSS Handler: Fetches and parses the RSS feed, then returns the items
+func fetchRSSHandler(w http.ResponseWriter, r *http.Request) {
+	feedURL := r.URL.Query().Get("feed_url")
+	if feedURL == "" {
+		http.Error(w, "Feed URL is required", http.StatusBadRequest)
+		return
+	}
+
+	// Fetch and parse the RSS feed
+	rssFeed, err := fetchRSSFeed(feedURL)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Failed to fetch RSS feed: %v", err), http.StatusInternalServerError)
+		return
+	}
+
+	// Render the parsed feed items in the HTML template
+	tmpl.ExecuteTemplate(w, "rssItems", rssFeed.Channel.Items)
+}
+
 func main() {
 	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/fetch-rss", fetchRSSHandler)
 
 	// Serve static files (e.g., CSS)
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
